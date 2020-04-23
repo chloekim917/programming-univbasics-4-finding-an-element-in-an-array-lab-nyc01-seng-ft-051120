7d482cd4ffa384befa3fb664febd40f4d17f6807
Seoyoung Kim <mintholic1@gmail.com>
2020-04-23 16:50:35 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  i = 0 
+  j = nil
+  while i<array.length do
+    if array[i] == value_to_find
+      puts i 
+      i += 1 
+    else puts j
+end
+end
 end
